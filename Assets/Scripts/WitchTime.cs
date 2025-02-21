168820e85abd6e55dd3d82c78bca5b3d8c26e792
jfeito26 <jorgefeicar26@gmail.com>
2025-02-21 16:33:01 +0100

    Apartado3

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
+    //public AudioClip WitchTime
+    //private bool isSlowed = false;
+    //public float howSlow = 0.25f;
     // Update is called once per frame
     void Update()
     {
-
+        //if(Input.GetKeyDown(KeyCode.Mouse1))
+        //{
+        //    StartCoroutine(SlowDown);
+        //}
     }
+    //private IEnumerator SlowDown()
+    //{
+    //    isSlowed = true;
+    //    Time.timeScale = howSlow;
+    //    Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    //    yield return new WaitForSeconds(howSlow);
+    //    Time.timeScale = 1;
+    //    Time.fixedDeltaTime = 0.02f;
+    //    isSlowed=false;
+    //    AudioManager.instance.PlayAudio(WitchTime, "WitchTime", true);
+    //}
 }
